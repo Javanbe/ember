c44012cea8f07dcb4d8cee8a5bd6e33029c8e3c5
javanbe <javanbe@outlook.com>
2017-05-30 04:00:05 +0800

    commit

--- app/routes/userinfo.js
+++ app/routes/userinfo.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-	return[{
-	    userName:'admin',
-	    number:'00000000000000',
-	    balance:0,
-	    abalance:0,
-	    currence:'$'
+	return[{userName:'admin',
+                number:'00000000000000',
+                balance:0,
+                abalance:0,
+                currence:'$'
 	}]
     }
 });
